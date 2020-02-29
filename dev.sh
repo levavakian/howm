5f04a12f2cb4994cebd8b1ac49afa30f15171c37
cjds <cjds@live.com>
2020-02-29 12:58:51 -0800

    bin bash

--- dev.sh
+++ dev.sh
@@ -1,1 +1,2 @@
+#!/bin/bash
 docker run -itd --network=host --name rowmc -v $(pwd)/../rowm:/go/src/github.com/levavakian/rowm/ golang:latest bash
--- docker-install.sh
+++ docker-install.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 set -e
 
 SCRIPT=$(readlink -f "$0")
