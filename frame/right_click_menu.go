0fd722c387c1a4933804f0034f4fae5d58347438
cjds <cjds@live.com>
2021-01-19 19:14:11 -0800

    right click

--- frame/right_click_menu.go
+++ frame/right_click_menu.go
@@ -25,6 +25,7 @@ type RightClickMenu struct {
 	maximization func(ctx *Context)
 	minimization func(ctx *Context)
 	always_on_top func(ctx* Context)
+	bBack, bTop, bMid, bBot *xwindow.Window
 }
 
 func NewRightClickMenu(ctx *Context) *RightClickMenu {
@@ -32,9 +33,11 @@ func NewRightClickMenu(ctx *Context) *RightClickMenu {
 	cwin := func(p xproto.Window) *xwindow.Window {
 		return xwindow.Must(xwindow.Create(ctx.X, p))
 	}
-	t.Maximize = cwin(ctx.X.RootWin())
-	t.Minimize = cwin(ctx.X.RootWin())
-	t.AlwaysOnTop = cwin(ctx.X.RootWin())
+	rootWin := ctx.X.RootWin()
+	t.bBack = cwin(rootWin)
+	t.Maximize = cwin(rootWin)
+	t.Minimize = cwin(rootWin)
+	t.AlwaysOnTop = cwin(rootWin)
 	var err error
 
 	// Base background
@@ -42,10 +45,6 @@ func NewRightClickMenu(ctx *Context) *RightClickMenu {
 	if err != nil {
 		log.Fatal(err)
 	}
-	t.Base.Window.Map()
-	t.Maximize.Map()
-	t.Minimize.Map()
-	t.AlwaysOnTop.Map()
 	mousebind.ButtonPressFun(
 		func(X *xgbutil.XUtil, ev xevent.ButtonPressEvent) {
 			t.maximization(ctx)
@@ -61,57 +60,83 @@ func NewRightClickMenu(ctx *Context) *RightClickMenu {
 			t.always_on_top(ctx)
 			t.Destroy()
 		}).Connect(ctx.X, t.AlwaysOnTop.Id, ctx.Config.ButtonClick, false, true)
+
+	t.bTop, t.bMid, t.bBot = cwin(rootWin), cwin(rootWin), cwin(rootWin)
+	cclr := func(w *xwindow.Window, clr render.Color) {
+		w.Change(xproto.CwBackPixel, clr.Uint32())
+	}
+	cclr(t.bBack, DefaultRightClickTheme.BgColor)
+	cclr(t.bTop, DefaultRightClickTheme.BorderColor)
+	cclr(t.bMid, DefaultRightClickTheme.BorderColor)
+	cclr(t.bBot, DefaultRightClickTheme.BorderColor)
+
+	t.bBack.Map()
+	t.Base.Window.Map()
+	t.Maximize.Map()
+	t.Minimize.Map()
+	t.AlwaysOnTop.Map()
+	t.bTop.Map()
+	t.bMid.Map()
+	t.bBot.Map()
+
 	return t
 }
 
 func (r *RightClickMenu) Show(x int16, y int16, canceled func(inp *RightClickMenu), maximization func(ctx *Context), minimization func(ctx *Context), always_on_top func(ctx* Context)) {
-	r.Maximize.Move(int(x), int(y))
-	r.Minimize.Move(int(x), DefaultRightClickTheme.Padding + int(y))
-	r.AlwaysOnTop.Move(int(x), 2 * DefaultRightClickTheme.Padding + int(y))
+	left := int(x)
+	top := int(y)
+	padding := DefaultRightClickTheme.Padding
+	bs := DefaultRightClickTheme.BorderSize
+	width := DefaultRightClickTheme.MenuWidth
+	r.bBack.MoveResize(left, top, width, top + 5*bs + 2*padding)
+	r.bTop.MoveResize(left, top + padding + bs, width, bs)
+	r.Maximize.MoveResize(left, top + bs, width, padding)
+	r.bMid.MoveResize(left, top + 2*padding + 2*bs, width, bs)
+	r.Minimize.Move(left, top + 2*bs + padding)
+	r.bBot.MoveResize(left, top + 3*padding + 3*bs, width, bs)
+	r.AlwaysOnTop.Move(left, top + 2*bs + 2*padding)
+
 	r.canceled = canceled
 	r.maximization = maximization
 	r.minimization = minimization
 	r.always_on_top = always_on_top
 	text.DrawText(r.Maximize, DefaultRightClickTheme.Font, DefaultRightClickTheme.FontSize,
-		DefaultRightClickTheme.FontColor, DefaultRightClickTheme.BgColor, "Maximize     ")
+		DefaultRightClickTheme.FontColor, DefaultRightClickTheme.BgColor, "Maximize")
 	text.DrawText(r.Minimize, DefaultRightClickTheme.Font, DefaultRightClickTheme.FontSize,
-		DefaultRightClickTheme.FontColor, DefaultRightClickTheme.BgColor, "Minimize     ")
+		DefaultRightClickTheme.FontColor, DefaultRightClickTheme.BgColor, "Minimize")
 	text.DrawText(r.AlwaysOnTop, DefaultRightClickTheme.Font, DefaultRightClickTheme.FontSize,
-		DefaultRightClickTheme.FontColor, DefaultRightClickTheme.BgColor, "Always On Top")
+		DefaultRightClickTheme.FontColor, DefaultRightClickTheme.BgColor, "On Top  ")
 }
 
 func (r *RightClickMenu) Destroy() {
 	r.Maximize.Destroy()
 	r.Minimize.Destroy()
 	r.AlwaysOnTop.Destroy()
+	r.bTop.Destroy()
+	r.bMid.Destroy()
+	r.bBot.Destroy()
 }
 
 
 type RightClickTheme struct {
-	BorderSize  int
 	BgColor     render.Color
-	InvisibleBgColor  render.Color
 	BorderColor render.Color
 	Padding     int
+	BorderSize int
 	Font      *truetype.Font
 	FontSize  float64
 	FontColor render.Color
-	SuggestionColor render.Color
-
-	InputWidth int
+	MenuWidth int
 }
 
 var DefaultRightClickTheme = &RightClickTheme{
-	BorderSize:  5,
 	BgColor:     render.NewImageColor(color.RGBA{0xff, 0xff, 0xff, 0xff}),
-	InvisibleBgColor:     render.NewImageColor(color.RGBA{0xff, 0xff, 0xff, 0x00}),
 	BorderColor: render.NewImageColor(color.RGBA{0x0, 0x0, 0x0, 0xff}),
-	Padding:     10,
-
+	Padding:     17,
+	BorderSize: 2,
 	Font: xgraphics.MustFont(xgraphics.ParseFont(
 		bytes.NewBuffer(misc.DataFile("DejaVuSans.ttf")))),
-	FontSize:   10.0,
+	FontSize:   15.0,
 	FontColor:  render.NewImageColor(color.RGBA{0x0, 0x0, 0x0, 0xff}),
-	SuggestionColor:  render.NewImageColor(color.RGBA{0xff, 0x0, 0x0, 0xff}),
-	InputWidth: 500,
+	MenuWidth: 100,
 }
