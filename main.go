ec44667fa459c97947f212cdafcd66ea353c6a09
cjds <cjds@live.com>
2021-05-03 08:05:55 -0400

    log

--- main.go
+++ main.go
@@ -19,13 +19,14 @@ import (
 )
 
 func main() {
-	logFile, err := os.OpenFile("/var/log/rowm.log", os.O_CREATE | os.O_APPEND | os.O_RDWR, 0666)
-	if err != nil {
-	    panic(err)
-	}
-	mw := io.MultiWriter(os.Stdout, logFile)
-	log.SetOutput(mw)
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	logFile, err := os.OpenFile("/var/log/rowm/rowm.log", os.O_CREATE | os.O_APPEND | os.O_RDWR, 0777)
+	if err != nil {
+	    log.Println(err)
+	} else {
+	  mw := io.MultiWriter(os.Stdout, logFile)
+	  log.SetOutput(mw)
+	}
 	log.Println("RoWM Window Manager")
 	log.Println("Hybrid Floating and Tiling Window Manager")
 	log.Println("Carbonated for your pleasure")
